e91764348a9e77fc940366a2083a47aa5f2e7eda
AnuradhaSonawane07 <anurson.sc@gmail.com>
2026-07-08 10:07:42 +0530

    Add JWT authentication middleware and protected profile API

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const userRoutes = require("./routes/userRoutes");
+
 const authRoutes = require("./routes/authRoutes"); // <-- Import FIRST
 
 const app = express();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes); // <-- Use AFTER importing
+app.use("/api/users", userRoutes);
 
 // MongoDB Connection
 mongoose
